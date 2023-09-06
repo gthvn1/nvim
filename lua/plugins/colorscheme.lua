bd7c12642912ba7041c7e4906198b512380a5466
Guillaume <guillaume.thouvenin@vates.tech>
2023-09-06 17:39:20 +0200

    colorscheme: change to catppuccin mocha

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,12 +2,25 @@ return {
     -- colorscheme
     { 'dracula/vim' },
     { 'rmehri01/onenord.nvim' },
+    { 'rose-pine/neovim' },
     {
-         'rose-pine/neovim',
-         lazy = false,
-         priority = 1000,
-         config = function ()
-             vim.cmd([[colorscheme rose-pine-moon]])
-         end,
-    }
+        'catppuccin/nvim',
+        name = 'catppuccin',
+        lazy = false, -- make sure we load this during startup if it is your main colorscheme
+        priority = 1000, -- make sure to load this before all the other start plugins
+        config = function()
+            require("catppuccin").setup({
+                integrations = {
+                    alpha = true,
+                    gitsigns = true,
+                    nvimtree = true,
+                    notify = true,
+                    treesitter = true,
+                    which_key = true,
+                },
+            })
+
+            vim.cmd([[colorscheme catppuccin-mocha]])
+        end
+    },
 }
