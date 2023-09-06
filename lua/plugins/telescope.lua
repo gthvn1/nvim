13fc7d50ca169d744114f4acc929c8d24d81047a
Guillaume <guillaume.thouvenin@vates.tech>
2023-09-06 09:24:20 +0200

    telescope: update keymaps

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,10 +5,11 @@ return {
         },
         config = function ()
             local builtin = require('telescope.builtin')
+            vim.keymap.set('n', '<leader><space>', builtin.buffers, {})
             vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
             vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-            vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
             vim.keymap.set('n', '<leader>fs', builtin.lsp_document_symbols, {})
+            vim.keymap.set('n', '<leader>ft', builtin.tags, {})
             vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
         end,
 }
