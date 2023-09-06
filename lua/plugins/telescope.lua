e35dd8a15bf49a5928ddee8ab612b0bc1d41b2f6
Guillaume <guillaume.thouvenin@vates.tech>
2023-09-06 10:29:38 +0200

    telescope: find dropdown

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,7 +4,17 @@ return {
                 'nvim-lua/plenary.nvim',
         },
         config = function ()
+            local telescope = require('telescope')
             local builtin = require('telescope.builtin')
+
+            telescope.setup({
+                pickers = {
+                    find_files = {
+                        theme = "dropdown",
+                    },
+                },
+            })
+
             vim.keymap.set('n', '<leader><space>', builtin.buffers, {})
             vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
             vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
