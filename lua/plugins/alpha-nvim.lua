63ea7aa4d7beba0adb6db52b5d4802808f7a11a8
Guillaume <guillaume.thouvenin@vates.tech>
2023-09-06 17:19:13 +0200

    plugins: alpha, add cool logo

--- lua/plugins/alpha-nvim.lua
+++ lua/plugins/alpha-nvim.lua
@@ -3,6 +3,23 @@ return {
     dependencies = {
         'nvim-tree/nvim-web-devicons'
     },
+    opts = function ()
+        local dashboard = require("alpha.themes.startify")
+        local logo = [[
+      ___           ___           ___           ___           ___     
+     /\  \         /\  \         /\__\         /\__\         /\__\    
+    /::\  \        \:\  \       /:/  /        /:/  /        /::|  |   
+   /:/\:\  \        \:\  \     /:/__/        /:/  /        /:|:|  |   
+  /:/  \:\  \       /::\  \   /::\  \ ___   /:/__/  ___   /:/|:|  |__ 
+ /:/__/_\:\__\     /:/\:\__\ /:/\:\  /\__\  |:|  | /\__\ /:/ |:| /\__\
+ \:\  /\ \/__/    /:/  \/__/ \/__\:\/:/  /  |:|  |/:/  / \/__|:|/:/  /
+  \:\ \:\__\     /:/  /           \::/  /   |:|__/:/  /      |:/:/  / 
+   \:\/:/  /     \/__/            /:/  /     \::::/__/       |::/  /  
+    \::/  /                      /:/  /       ~~~~           /:/  /   
+     \/__/                       \/__/                       \/__/    
+]]
+        dashboard.section.header.val = vim.split(logo, "\n")
+    end,
     config = function ()
         require('alpha').setup(require('alpha.themes.startify').config)
     end
