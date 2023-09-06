783722cbe460c9154f0fb8a5ff7b319aad2faa8c
Guillaume <guillaume.thouvenin@vates.tech>
2023-09-06 18:09:44 +0200

    plugins: set colorscheme within core.lua

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,24 +3,8 @@ return {
     { 'dracula/vim' },
     { 'rmehri01/onenord.nvim' },
     { 'rose-pine/neovim' },
+    { 'catppuccin/nvim' },
     {
-        'catppuccin/nvim',
-        name = 'catppuccin',
-        lazy = false, -- make sure we load this during startup if it is your main colorscheme
-        priority = 1000, -- make sure to load this before all the other start plugins
-        config = function()
-            require("catppuccin").setup({
-                integrations = {
-                    alpha = true,
-                    gitsigns = true,
-                    nvimtree = true,
-                    notify = true,
-                    treesitter = true,
-                    which_key = true,
-                },
-            })
-
-            vim.cmd([[colorscheme catppuccin-mocha]])
-        end
+        'folke/tokyonight.nvim',
     },
 }
