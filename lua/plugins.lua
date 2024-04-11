a07262ca4a593d73f846889a44f66cd3a127de7e
Guillaume <gthvn1@gmail.com>
2024-04-11 20:14:32 +0200

    add dracula theme

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -3,6 +3,9 @@ local Plug = vim.fn['plug#']
 
 vim.call('plug#begin')
 
+Plug('folke/tokyonight.nvim')
+Plug('dracula/vim')
+
 Plug('ChrisWellsWood/roc.vim')
 Plug('nvim-telescope/telescope.nvim')
 Plug('nvim-lua/plenary.nvim')
@@ -10,7 +13,6 @@ Plug('BurntSushi/ripgrep')
 Plug('nvim-treesitter/nvim-treesitter', { ['tag'] = '0.1.6' })
 Plug('nvim-lualine/lualine.nvim')
 Plug('nvim-tree/nvim-web-devicons')
-Plug('folke/tokyonight.nvim')
 Plug('williamboman/mason.nvim')
 Plug('neovim/nvim-lspconfig')
 
