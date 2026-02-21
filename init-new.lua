bb803e8c9a7dd6d1c7ee124da3da5024f47094ac
Guillaume <guillaume.thouvenin@vates.tech>
2026-02-21 19:13:46 +0100

    add iron to manage ocaml repl

--- init-new.lua
+++ init-new.lua
@@ -36,6 +36,7 @@ require("lazy").setup({
 	"oskarnurm/koda.nvim", -- theme
 	"lucasadelino/conifer.nvim", -- theme
 	"lewis6991/gitsigns.nvim", -- Git signs
+	"Vigemus/iron.nvim", -- repl
 	{ -- fuzzy finder
 		"nvim-telescope/telescope.nvim",
 		dependencies = { "nvim-lua/plenary.nvim" },
@@ -172,6 +173,102 @@ vim.lsp.config("julials", {
 	end,
 })
 vim.lsp.enable("julials")
+
+-- ===============================
+-- Iron (REPL) setup
+-- ===============================
+local iron = require("iron.core")
+local view = require("iron.view")
+local common = require("iron.fts.common")
+
+iron.setup({
+	config = {
+		scratch_repl = true,
+		repl_definition = {
+			ocaml = {
+				command = { "utop" },
+				block_dividers = { "(* %% *)" },
+				format = function(lines)
+					-- Add ;; to the last line if missing
+					if #lines > 0 then
+						local last = lines[#lines]
+
+						if not last:match(";;%s*$") then
+							lines[#lines] = last .. ";;"
+						end
+					end
+
+					table.insert(lines, "")
+
+					return lines
+				end,
+			},
+		},
+		-- set the file type of the newly created repl to ft
+		-- bufnr is the buffer id of the REPL and ft is the filetype of the
+		-- language being used for the REPL.
+		repl_filetype = function(bufnr, ft)
+			return ft
+			-- or return a string name such as the following
+			-- return "iron"
+		end,
+		-- Send selections to the DAP repl if an nvim-dap session is running.
+		dap_integration = true,
+		-- How the repl window will be displayed
+		-- See below for more information
+		repl_open_cmd = view.bottom(40),
+
+		-- repl_open_cmd can also be an array-style table so that multiple
+		-- repl_open_commands can be given.
+		-- When repl_open_cmd is given as a table, the first command given will
+		-- be the command that `IronRepl` initially toggles.
+		-- Moreover, when repl_open_cmd is a table, each key will automatically
+		-- be available as a keymap (see `keymaps` below) with the names
+		-- toggle_repl_with_cmd_1, ..., toggle_repl_with_cmd_k
+		-- For example,
+		--
+		-- repl_open_cmd = {
+		--   view.split.vertical.rightbelow("%40"), -- cmd_1: open a repl to the right
+		--   view.split.rightbelow("%25")  -- cmd_2: open a repl below
+		-- }
+	},
+	-- Iron doesn't set keymaps by default anymore.
+	-- You can set them here or manually add keymaps to the functions in iron.core
+	keymaps = {
+		toggle_repl = "<space>rr", -- toggles the repl open and closed.
+		-- If repl_open_command is a table as above, then the following keymaps are
+		-- available
+		-- toggle_repl_with_cmd_1 = "<space>rv",
+		-- toggle_repl_with_cmd_2 = "<space>rh",
+		restart_repl = "<space>rR", -- calls `IronRestart` to restart the repl
+		send_motion = "<space>sc",
+		visual_send = "<space>sc",
+		send_file = "<space>sf",
+		send_line = "<space>sl",
+		send_paragraph = "<space>sp",
+		send_until_cursor = "<space>su",
+		send_mark = "<space>sm",
+		send_code_block = "<space>sb",
+		send_code_block_and_move = "<space>sn",
+		mark_motion = "<space>mc",
+		mark_visual = "<space>mc",
+		remove_mark = "<space>md",
+		cr = "<space>s<cr>",
+		interrupt = "<space>s<space>",
+		exit = "<space>sq",
+		clear = "<space>cl",
+	},
+	-- If the highlight is on, you can change how it looks
+	-- For the available options, check nvim_set_hl
+	highlight = {
+		italic = true,
+	},
+	ignore_blank_lines = true, -- ignore blank lines when sending visual select lines
+})
+
+-- iron also has a list of commands, see :h iron-commands for all available commands
+vim.keymap.set("n", "<space>rf", "<cmd>IronFocus<cr>")
+vim.keymap.set("n", "<space>rh", "<cmd>IronHide<cr>")
 
 -- ===============================
 -- Completion setup (nvim-cmp)
